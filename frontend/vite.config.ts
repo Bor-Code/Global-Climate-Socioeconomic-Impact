07aa2a363fec117fc6592f664211a8a7b78cd4f5
Boran <non.mrbora@gmail.com>
2026-08-28 01:17:01 +0300

    feat(ui): convert react application to module federation micro-frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,24 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'climate_dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App.tsx',
+      },
+      shared: ['react', 'react-dom']
+    })
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
 })
